deb2ae8de5efa7b503da8bfa4456c86d2ac87e8f
Ruling <rulingfong@outlook.com>
2017-03-02 22:24:54 +0800

    everything is ready

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,6 @@
 var router = require('koa-router')();
 
 router.get('/', async function (ctx, next) {
-  ctx.state = {
-    title: 'koa2 title'
-  };
-
-  await ctx.render('index', {
-  });
+  ctx.body = 'Hello Koa!';
 })
 module.exports = router;
